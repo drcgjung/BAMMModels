f967d06656114a9cb40bf96a2230cd6289d589e7
Dr. Christoph 'Schorsch' Jung <c-jung@t-systems.com>
2021-12-15 15:53:24 +0100

    https://jira.catena-x.net/browse/A1SLDT-234 Resolve review comments and debug technical data escape in description text.

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -11,11 +11,15 @@
 #
 # Shell script to perform a deployment of semantic models
 #
-# Syntax ./deploy.ssh (-delete|-draft|-deprecate|-release) PATH_TO_MODEL_FILE*
-#            -delete delete the models associated to the given paths
-#            -draft create or update the models described in the given paths with status DRAFT (default)
-#            -release create or update the models described in the given paths with status RELEASED
-#            -deprecate create or update the models described in the given paths with status DEPRECATED
+# Syntax ./deploy.sh (-delete|-draft|-deprecate|-release|PATH_TO_MODEL_FILE)*
+#            -delete delete the models associated to the following paths
+#            -draft create or update the models described in the following paths with status DRAFT (default)
+#            -release create or update the models described in the following paths with status RELEASED
+#            -deprecate create or update the models described in the following paths with status DEPRECATED
+#
+# Note: In all non-delete modes (-draft|-release|-deprecate), the existance of the term "deprecated" in the path name of a follwing model will
+# lead to its deployment under status DEPRECATED 
+# Note: paths not ending with a turtle file (*.ttl) are ignored
 #
 # (Optional) Environment variables
 #            - WORKSPACE the target environment (default: dev042)
@@ -42,91 +46,133 @@ SUCCESS="true"
 DELETION="false"
 MODEL_STATUS="DRAFT"
 
-for model in "$@"
+for argument in "$@"
 do
-  if [[ "${model}" == "-delete" ]]; then
+  #switch on delete mode
+  if [[ "${argument}" == "-delete" ]]; then
     DELETION="true"
-  elif [[ "${model}" == "-draft" ]]; then
+
+  # switch on draft mode
+  elif [[ "${argument}" == "-draft" ]]; then
     DELETION="false"; MODEL_STATUS="DRAFT"
-  elif [[ "${model}" == "-release" ]]; then
+  
+  # switch on release mode
+  elif [[ "${argument}" == "-release" ]]; then
     DELETION="false"; MODEL_STATUS="RELEASED"
-  elif [[ "${model}" == "-deprecate" ]]; then
+  
+  # switch on deprecation mode
+  elif [[ "${argument}" == "-deprecate" ]]; then
     DELETION="false"; MODEL_STATUS="DEPRECATED"
-  elif [[ "${model}" == *.ttl ]]; then
+  
+  # is the path a ttl file?
+  elif [[ "${argument}" == *.ttl ]]; then
+
+    # are we in delete or modify/create mode?
     if [[ "${DELETION}" == "false" ]]; then
-        if [[ "${model}" == *deprecated* ]]; then
+
+        # should we apply auto-deprecation because of the (moved) path name
+        if [[ "${argument}" == *deprecated* ]]; then
           CURRENT_MODEL_STATUS="DEPRECATED"
         else
           CURRENT_MODEL_STATUS=${MODEL_STATUS}
         fi
-        echo "About to deploy ${model} to ${SEMANTIC_HUB} in status ${CURRENT_MODEL_STATUS}"
-        MODEL_NAMESPACE=$(cat ${model} | grep -o -E '@prefix : <[^"]+#>.' |sed  -n 's/^@prefix : <\(.*\)#>.$/\1/p')
-        MODEL_NAME=$(cat ${model} | grep -o -E ':[^"]+ a bamm:Aspect' |sed  -n 's/^:\(.*\) a bamm:Aspect$/\1/p')
+
+        # Perform a fresh create try
+        echo "About to deploy ${argument} to ${SEMANTIC_HUB} in status ${CURRENT_MODEL_STATUS}"
+        
+        MODEL_NAMESPACE=$(cat ${argument} | grep -o -E '@prefix : <[^"]+#>.' |sed  -n 's/^@prefix : <\(.*\)#>.$/\1/p')
+        MODEL_NAME=$(cat ${argument} | grep -o -E ':[^"]+ a bamm:Aspect' |sed  -n 's/^:\(.*\) a bamm:Aspect$/\1/p')
         MODEL_ID="${MODEL_NAMESPACE}#${MODEL_NAME}"
+        
         echo "Deduced model id ${MODEL_ID}"
-        #cat ${model} | sed ${REPLACE_ORG_NEWLINE} | sed ${REPLACE_QUOTE}  | sed ${REPLACE_NEWLINE} > test.json
-        STATUS=$(echo '{"private": false, "publisher":"'${PUBLISHER}'", "status":"'${CURRENT_MODEL_STATUS}'", "type": "BAMM", "model":"' $( cat ${model} | sed ${REPLACE_ORG_NEWLINE} | sed ${REPLACE_QUOTE}  | sed ${REPLACE_NEWLINE} ) '"}' | \
+        
+        #cat ${argument} | sed ${REPLACE_ORG_NEWLINE} | sed ${REPLACE_QUOTE}  | sed ${REPLACE_NEWLINE} > test.json
+        STATUS=$(echo '{"private": false, "publisher":"'${PUBLISHER}'", "status":"'${CURRENT_MODEL_STATUS}'", "type": "BAMM", "model":"' $( cat ${argument} | sed ${REPLACE_ORG_NEWLINE} | sed ${REPLACE_QUOTE}  | sed ${REPLACE_NEWLINE} ) '"}' | \
             curl -s --location --request POST 'https://'${SEMANTIC_HUB} \
                 --header 'Content-Type: application/json' \
                 -d @- -w '%{http_code}')
 #    --header 'Authorization: Basic dXNlcjpwYXNzd29yZA=='
+
         echo "Got creation status ${STATUS}"
+
+        # Model already existed?
         if [[ "${STATUS}" == *200 ]]; then
             echo "Model ${MODEL_ID} was successfully created.";
         elif [[ "${STATUS}" == *400 ]]; then
+            
+            # Perform a scond modification try
             echo "Model ${MODEL_ID} already exists. Try to modify.";
-            STATUS=$(echo '{ "private": false, "publisher":"'${PUBLISHER}'", "status":"'${CURRENT_MODEL_STATUS}'", "type": "BAMM", "model":"' $( cat ${model} | sed ${REPLACE_ORG_NEWLINE} | sed ${REPLACE_QUOTE}  | sed ${REPLACE_NEWLINE} ) '"}' | \
+            STATUS=$(echo '{ "private": false, "publisher":"'${PUBLISHER}'", "status":"'${CURRENT_MODEL_STATUS}'", "type": "BAMM", "model":"' $( cat ${argument} | sed ${REPLACE_ORG_NEWLINE} | sed ${REPLACE_QUOTE}  | sed ${REPLACE_NEWLINE} ) '"}' | \
                 curl -s --location --request PUT 'https://'${SEMANTIC_HUB} \
                 --header 'Content-Type: application/json' \
                 -d @- -w '%{http_code}')
 #    --header 'Authorization: Basic dXNlcjpwYXNzd29yZA=='
+
             echo "Got modification status ${STATUS}"
+
             if [[ "${STATUS}" == *200 ]]; then
                 echo "Model ${MODEL_ID} was successfully updated.";
-            else 
+            else
+
+                # Failure conditition for modification call 
                 echo "Unknown modification status ${STATUS}. Marking deployment as failed";
                 SUCCESS="false"
             fi
+
         else
+            # Failure condition for creation call
             echo "Unknown creation status ${STATUS}. Marking deployment as failed."
             SUCCESS="false"
         fi
+
+
     else
+
+        # This is the delete mode, since the file does no exist anymore, we
+        # need to derive all important information from the path name
+
         REGEX="(.*)/([0-9]+\.[0-9]+\.[0-9]+)/([^/]+).ttl"
-        if [[ ${model} =~ ${REGEX} ]]; then
+        if [[ ${argument} =~ ${REGEX} ]]; then
+
+          # Perform a list query on the relevant models
           MODEL_NAME=${BASH_REMATCH[3]}
           MODEL_VERSION=${BASH_REMATCH[2]}
-          echo "About to delete ${model} in ${SEMANTIC_HUB}. Assuming MODEL_NAME ${MODEL_NAME} and version ${MODEL_VERSION}"
+          echo "About to delete ${argument} in ${SEMANTIC_HUB}. Assuming MODEL_NAME ${MODEL_NAME} and version ${MODEL_VERSION}"
           JQ_QUERY=".[]|select(.id|endswith(\"${MODEL_VERSION}#${MODEL_NAME}\"))|.id"
           URL="https://${SEMANTIC_HUB}?nameFilter=${MODEL_NAME}&namespaceFilter=${MODEL_VERSION}"
           MODELS_TO_DELETE=($(curl -s --location --request GET ${URL} | jq -r $JQ_QUERY))
 #    --header 'Authorization: Basic dXNlcjpwYXNzd29yZA=='
           for element in ${MODELS_TO_DELETE[@]}
           do
-            echo $element
+
+            # Delete each of the relevant models
             MODEL_TO_DELETE=${element//[$'\t\r\n']} 
             URLENCODED_MODEL_TO_DELETE=${MODEL_TO_DELETE//#/%23}
             echo "About to delete ${URLENCODED_MODEL_TO_DELETE} in ${SEMANTIC_HUB}"
+
             URL="https://${SEMANTIC_HUB}/${URLENCODED_MODEL_TO_DELETE}"
             STATUS=$(curl -s --location --request DELETE ${URL} -w '%{http_code}')
 #    --header 'Authorization: Basic dXNlcjpwYXNzd29yZA=='
+            
             echo "Got deletion status ${STATUS}"
             if [[ "${STATUS}" == *204 ]]; then
               echo "Model ${URLENCODED_MODEL_TO_DELETE} was successfully deleted.";
             else 
+              # Failure condition on deletion
               echo "Unknown deletion status ${STATUS}. Marking deployment as failed";
               SUCCESS="false"
             fi
           done
         else
+          # Ignore paths without proper naming
           echo "Model path ${MODEL_NAME} does not conform to the MODEL_NAME/VERSION/MODEL_NAME.ttl standard. The model cannot be looked up/deleted."
         fi
     fi
   else
-       echo "Ignoring file ${model} as it is no ttl file."
+       # Ignore paths without proper naming
+       echo "Ignoring file ${argument} as it is no ttl file."
   fi
 done
-
 
 if [[ "$SUCCESS" == "true" ]]; then
   exit 0
